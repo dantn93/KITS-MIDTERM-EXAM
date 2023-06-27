6025ad34202ce7029dfb233c31916bfc3703683e
DnSonAnh <dnsonanh@gmail.com>
2023-06-27 15:41:17 +0700

    String

---
+++ example/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(takeHeadAndTail("abc"))); // ["a", "c"]
+        System.out.println(Arrays.toString(takeHeadAndTail("a"))); // ["a", "a"]
+        System.out.println(Arrays.toString(takeHeadAndTail(""))); // ["", ""]
+    }
+
+    public static String[] takeHeadAndTail(String str) {
+        if (str.length() == 0) {
+            return new String[]{"", ""};
+        } else {
+            char firstChar = str.charAt(0);
+            char lastChar = str.charAt(str.length() - 1);
+            return new String[]{Character.toString(firstChar), Character.toString(lastChar)};
+        }
+    }
+}
+
