bda935053e395a6150e5494b09f51a3cc9b5b345
thainguyen2k <nguyendragon2000@gmail.com>
2023-06-27 16:08:01 +0700

    nop bai

---
+++ baitap2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class baitap2 {
+    public static void main(String[] args) {
+        
+    
+
+    }
+    
+    public static List<String> toList(String[] array) {
+    List<String> convert = new ArrayList<>();
+    for (String element : array) {
+        convert.add(element);
+    }
+    return convert;
+    }
+    
+}
